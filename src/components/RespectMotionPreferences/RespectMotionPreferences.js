63aabf040e6dd23d1435ae3bc3f6b51e363b2bb0
Andrew Nessin <andrewnessinjim@yahoo.com>
2024-02-10 14:08:00 +0530

    Exercise 5b

---
+++ src/components/RespectMotionPreferences/RespectMotionPreferences.js
@@ -0,0 +1,14 @@
+"use client";
+
+import { MotionConfig } from "framer-motion";
+import React from "react";
+
+function RespectMotionPreferences({ children }) {
+  return (
+    <MotionConfig
+      reducedMotion="user">
+        {children}
+    </MotionConfig>);
+}
+
+export default RespectMotionPreferences;
